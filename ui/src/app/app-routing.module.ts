e2d257335fa118330afe1991a7c1e06f41ab9692
vijaygrandhi <vijayrahul.grandhi@gmail.com>
2023-10-21 12:29:33 +0530

    navbar with clicks

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { OrderComponent } from './order/order.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: 'contactus', component: ContactusComponent
+  },
+  {
+    path: 'order', component: OrderComponent
+  },
+  {
+    path:'login',component:LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
